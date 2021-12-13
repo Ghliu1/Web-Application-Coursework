8b873d218c4b9bf310b67d1b037d5707f9402fe0
Ghliu1 <georgeliu3@hotmail.com>
2021-12-13 18:15:20 +0000

    Made a controller and blade files

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+
+    <html>
+     <head>
+            <title>Posts</title>
+        </head>
+        <body>
+           <h1>{{$post ?? ''}} Posts</h1>
+          <p>Here are some posts by other students! Feel free to add to the discussions</p>
+        </body>
+    </html>
+</x-app-layout>
